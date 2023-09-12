872fb301240d63830ca3beaf050b04ad16d2be66
Archie Young <750826700@qq.com>
2023-09-12 16:05:53 +0800

    v1.3.0.0

--- src/main/java/org/bgi/flexlab/dpgt/jointcalling/JointCallingSparkOptions.java
+++ src/main/java/org/bgi/flexlab/dpgt/jointcalling/JointCallingSparkOptions.java
@@ -38,7 +38,7 @@ public class JointCallingSparkOptions implements Serializable {
     private HelpFormatter helpFormatter = new HelpFormatter();
 
     // program meta data
-    private static final String VERSION = "1.2.14.0-rv";
+    private static final String VERSION = "1.3.0.0";
 
     private ReferenceDataSource referenceDataSrc = null;
     private SAMSequenceDictionary sequenceDict = null;
